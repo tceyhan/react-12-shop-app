fd960129bf5dacfea4c2a2a9486e5693af1833ec
tceyhan <55tc155@gmail.com>
2022-08-27 20:05:20 +0300

    scss files updated

--- src/App.js
+++ src/App.js
@@ -2,10 +2,11 @@ import Card from "./components/card/Card";
 import Category from "./components/category/Category";
 import data from "./data";
 
+
 function App() {
   return (
     <>
-      <h1>shop-app </h1>
+      
       <Category/>
       <Card veri={data}/>
     </>
