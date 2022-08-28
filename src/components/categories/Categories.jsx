026c88967fad4c14a3efdb30d39aee858e9eff0e
tceyhan <55tc155@gmail.com>
2022-08-28 14:08:24 +0300

    css files removed

--- src/components/categories/Categories.jsx
+++ src/components/categories/Categories.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+
 import { Link, Outlet } from "react-router-dom";
 import SelectMenu from "../selectmenu/SelectMenu";
 import "./categories.scss";
@@ -8,9 +8,8 @@ const Categories = () => {
     <div className="home">
       <div className="d-flex justify-content-center">
         <img src="../../assets/images/catagory_link_picture/kahvaltılık.jpg" alt="" />
-        <Link to="breakfast" className="btn breakfast">
-          <img src="../../assets/images/catagory_link_picture/kahvaltılık.jpg" alt="" />          
-          <p>KAHVALTILIK</p>
+        <Link to="breakfast" className="btn breakfast">                 
+          KAHVALTILIK
         </Link>
         <Link to="organicfruits" className="btn organicfruits uppercase">
           ORGANİK ÜRÜN
--- src/components/categories/OrganicFruits.jsx
+++ src/components/categories/OrganicFruits.jsx
@@ -2,7 +2,14 @@ import React from 'react'
 
 const OrganicFruits = () => {
   return (
-    <div>Organic Fruits</div>
+    <div className="d-flex flex-column justify-content-center align-items-center">
+    <h1 className="header">Organik Ürünler</h1>
+    <img
+      className="img-fluid"
+      src="https://www.evimdekiegitim.com/assets/images/yapim.jpg"
+      alt="YAPIM AŞAMASINDA"
+    />
+  </div>
   )
 }
 
